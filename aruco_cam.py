539d11bced67c07a2d51affe57b8869f331754bd
HarryPretel <33470366+HarryPretel@users.noreply.github.com>
2019-11-17 17:01:23 -0600

    fixed some in aruco_cam

---
+++ aruco_cam.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+import cv2.aruco
+
+face_cascade = cv2.CascadeClassifier('../../../../../Miniconda3/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('../../../../../Miniconda3/Lib/site-packages/cv2/data/haarcascade_eye.xml')
+
+cap = cv2.VideoCapture(0);
+
+while(True):
+	ret, frame = cap.read()
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	
+	aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+	parameters = aruco.DetectorParameters_create()
+	corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+	frame_markers =  aruco.drawDetectedMarkers(frame.copy(), corners, ids)
+
+	cv2.imshow('img',frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+cv2.destroyAllWindows()
